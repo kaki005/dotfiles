3badb4cd44e5c55951c143bc3738fd996ce40075
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-07-28 17:33:58 +0900

    add bat command

--- dotfiles/zsh/plugin.zsh
+++ dotfiles/zsh/plugin.zsh
@@ -1,3 +1,11 @@
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 zinit light zdharma/fast-syntax-highlighting
 zinit light paulirish/git-open
+
+# bat command
+zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
+zinit light sharkdp/bat
+# 以下はただのエイリアス設定
+if builtin command -v bat > /dev/null; then
+  alias cat="bat"
+fi
