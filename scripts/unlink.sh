b299c52516423c9d0c27c5b372bb16106abf64be
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-07-28 17:10:40 +0900

    :tada: add dotfiles

--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -4,17 +4,15 @@
 dotfiles_root=$(cd $(dirname $0)/.. && pwd)
 . ${dotfiles_root}/scripts/common.sh
 
-# シンボリックリンクを作成
+# シンボリックリンクを削除
 cd ${dotfiles_root}/dotfiles
 for linklist in "linklist.Unix.txt" "linklist.$(uname).txt"; do
     [ ! -r "${linklist}" ] && continue
 
     __remove_linklist_comment "$linklist" | while read target link; do
         # ~ や環境変数を展開
-        target=$(eval echo "${PWD}/${target}")
         link=$(eval echo "${link}")
         # シンボリックリンクを作成
-        __mkdir $(dirname ${link})
-        __ln ${target} ${link}
+        __unlink ${link}
     done
 done
