ba3938fd3a3f2470c346d2394243cf1d65c29288
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-07-28 19:51:56 +0900

    :heavy_plus_sign: [zsh] add plugin

--- dotfiles/zsh/plugin.zsh
+++ dotfiles/zsh/plugin.zsh
@@ -9,8 +9,16 @@ zinit light sharkdp/bat
 # 以下はただのエイリアス設定
 if builtin command -v bat > /dev/null; then
   alias cat="bat"
-fi
+fii
 
+# exa
+zinit ice as"program" from"gh-r" mv"eza* -> eza"
+zinit light eza-community/eza
+
+# riggrep
+zinit ice as"program" from"gh-r" mv"ripgrep* -> rg" pick"rg/rg"
+zinit light BurntSushi/ripgrep
+
+#fd
+zinit ice as"program" from"gh-r" mv"fd* -> fd" pick"fd/fd"
 zinit light sharkdp/fd
-zinit light wting/autojump
-
