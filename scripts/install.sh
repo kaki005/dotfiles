d3ee0e92da474f45a966f46ffa25ac03321cbde5
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-08-04 16:41:56 +0900

    add Brewfile

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+echo "Start setup ..."
+
+if [ $(uname) = Darwin ]; then
+    if ! type brew &> /dev/null ; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "Since Homebrew is already installed, skip this phase and proceed."
+    fi
+    brew bundle install --file=Brewfile
+fi
+
