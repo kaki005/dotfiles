d79933c4b467e5155ee91982bb976dadf11d1e8c
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-08-04 17:23:15 +0900

    add key-bind

--- dotfiles/zsh/key-bind.zsh
+++ dotfiles/zsh/key-bind.zsh
@@ -40,4 +40,14 @@ zle -N find_cd
 bindkey '^W' find_cd
 
 
-
+## ghqとの連携。ghqの管理化にあるリポジトリを一覧表示する。ctrl - ]にバインド。
+function peco-src () {
+  local selected_dir=$(ghq list -p | peco --prompt="repositories >" --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-src
+bindkey '^]' peco-src
