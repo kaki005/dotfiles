62e0a1c1093a6b0267ce6d0c95d90b9084f592f0
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-12-11 18:20:34 +0900

    update LESSKEY,RYEDIR

--- dotfiles/zsh/export.zsh
+++ dotfiles/zsh/export.zsh
@@ -1,7 +1,8 @@
 export EDITOR=nvim
-export NVM_DIR="$HOME/.nvm"
+export RYE_HOME="$HOME/.cache/rye"
+export NVM_DIR="$HOME/.cache/nvm"
 export HIST_DIR="$HOME/.cache/zsh/" # .zshの履歴を保存するファイル
-export LESSKEY=${DOTFILES_DIR}/dotfiles/less
+export LESSKEY=${DOTFILES_DIR}/dotfiles/less/lesskey
 export PYTHONSTARTUP="$DOTFILES_DIR/dotfiles/python/.pythonrc.py"
 export HISTSIZE=100000             # メモリ上に保存する履歴のサイズ
 export SAVEHIST=1000000            # 上述のファイルに保存する履歴のサイズ
