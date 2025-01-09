e3d508286ea1bc98215439436884fc19138a73e0
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-01-09 09:02:46 +0900

    [python] update pythonrc

--- dotfiles/python/.pythonrc.py
+++ dotfiles/python/.pythonrc.py
@@ -13,3 +13,38 @@ def my_displayhook(value):
       __builtins__._ = value
     pprint.pprint(value)
 sys.displayhook = my_displayhook
+
+
+## common standard library imports
+print('')
+from datetime import datetime as dt
+pprint.pprint('from datetime import datetime as dt')
+import sys, os, glob, re, subprocess, json, datetime, random, time
+pprint.pprint('import sys, os, glob, re, subprocess, json, datetime, random, time')
+from collections import namedtuple, defaultdict, Counter
+pprint.pprint('from collections import namedtuple, defaultdict, Counter')
+from itertools import product
+pprint.pprint('from itertools import product')
+from math import *
+pprint.pprint('from math import *')
+
+## common pip imports
+print('')
+try:
+    from dateutil.parser import parse
+    pprint.pprint('from dateutil.parser import parse')
+except:
+    pprint.pprint('dateutil not available!')
+try:
+    import numpy as np
+    pprint.pprint('import numpy as np')
+except:
+    pprint.pprint('numpy not available!')
+try:
+    import warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        import matplotlib.pyplot as plt
+        pprint.pprint('import matplotlib.pyplot as plt')
+except:
+    pprint.pprint('matplotlib not available!')
