de5e6279a4c959c79bc63037b6ba23c45f06a7ae
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-02-04 17:31:40 +0900

    update link.ps1

--- scripts/link.ps1
+++ scripts/link.ps1
@@ -1,7 +1,7 @@
 New-Item -ItemType SymbolicLink -Path $HOME -Name .vimrc -Value $HOME\dotfiles\dotfiles\vim\.vimrc
 New-Item -ItemType SymbolicLink -Path $HOME -Name .gitconfig -Value $HOME\dotfiles\dotfiles\git\.gitconfig
 New-Item -ItemType SymbolicLink -Path $HOME -Name .wezterm.lua -Value $HOME\dotfiles\dotfiles\wezterm\.wezterm.lua
-
+New-Item -ItemType SymbolicLink -Path C:\Windows\Fonts -Name 0xProtoNerdFont-Regular.ttf -Value $HOME\dotfiles\dotfiles\starship\fonts\0xProtoNerdFont-Regular.ttf
 # Vscode
 New-Item -ItemType SymbolicLink -Path $HOME\AppData\Roaming\Code\User -Name settings.json -Value $HOME\dotfiles\dotfiles\vscode\settings.json
 New-Item -ItemType SymbolicLink -Path $HOME\AppData\Roaming\Code\User -Name keybindings.json -Value $HOME\dotfiles\dotfiles\vscode\keybindings.json
