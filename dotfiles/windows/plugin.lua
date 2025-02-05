0afb32113a7379dfbb6ba758b7349a7de12db2ba
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-02-05 15:49:56 +0900

    modify clink

--- dotfiles/windows/plugin.lua
+++ dotfiles/windows/plugin.lua
@@ -1,1 +1,5 @@
+-- starship.lua
+load(io.popen('starship init cmd'):read("*a"))()
 os.setenv('STARSHIP_CONFIG', '$HOME\dotfiles\dotfiles\starship\starship.toml')
+
+io.writeline("plugin load completed.")
--- scripts/link.ps1
+++ scripts/link.ps1
@@ -3,9 +3,9 @@ New-Item -ItemType SymbolicLink -Path $HOME -Name .gitconfig -Value $HOME\dotfil
 New-Item -ItemType SymbolicLink -Path $HOME -Name .wezterm.lua -Value $HOME\dotfiles\dotfiles\wezterm\.wezterm.lua
 New-Item -ItemType SymbolicLink -Path C:\Windows\Fonts -Name 0xProtoNerdFont-Regular.ttf -Value $HOME\dotfiles\dotfiles\starship\fonts\0xProtoNerdFont-Regular.ttf
 New-Item -ItemType SymbolicLink -Path $HOME\Documents\PowerShell -Name Microsoft.PowerShell_profile.ps1 -Value $HOME/dotfiles/dotfiles/windows/Microsoft.PowerShell_profile.ps1
-New-Item -ItemType SymbolicLink -Path %localappdata%\clink -Name clink_settings -Value $HOME/dotfiles/dotfiles/windows/clink_settings
-New-Item -ItemType SymbolicLink -Path %localappdata%\clink -Name aliases -Value $HOME/dotfiles/dotfiles/window/aliases
-New-Item -ItemType SymbolicLin -Path %localappdata%\clink -Name plugin.lua -Value $HOME/dotfiles/dotfiles/windows/plugin.lua
+New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\clink -Name clink_settings -Value $HOME/dotfiles/dotfiles/windows/clink_settings
+New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\clink -Name aliases -Value $HOME/dotfiles/dotfiles/windows/aliases
+New-Item -ItemType SymbolicLin -Path $env:LOCALAPPDATA\clink -Name plugin.lua -Value $HOME/dotfiles/dotfiles/windows/plugin.lua
 # Vscode
 New-Item -ItemType SymbolicLink -Path $HOME\AppData\Roaming\Code\User -Name settings.json -Value $HOME\dotfiles\dotfiles\vscode\settings.json
 New-Item -ItemType SymbolicLink -Path $HOME\AppData\Roaming\Code\User -Name keybindings.json -Value $HOME\dotfiles\dotfiles\vscode\keybindings.json
