174169eca4ed0328f9426f84f91fd42381e1dc14
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-02-05 09:48:27 +0900

    [windows] add scripts

--- scripts/link.ps1
+++ scripts/link.ps1
@@ -2,6 +2,7 @@ New-Item -ItemType SymbolicLink -Path $HOME -Name .vimrc -Value $HOME\dotfiles\d
 New-Item -ItemType SymbolicLink -Path $HOME -Name .gitconfig -Value $HOME\dotfiles\dotfiles\git\.gitconfig
 New-Item -ItemType SymbolicLink -Path $HOME -Name .wezterm.lua -Value $HOME\dotfiles\dotfiles\wezterm\.wezterm.lua
 New-Item -ItemType SymbolicLink -Path C:\Windows\Fonts -Name 0xProtoNerdFont-Regular.ttf -Value $HOME\dotfiles\dotfiles\starship\fonts\0xProtoNerdFont-Regular.ttf
+New-Item -ItemType SymbolicLink -Path $HOME\Documents\PowerShell -Name Microsoft.PowerShell_profile.ps1 -Value $HOME/dotfiles/dotfiles/windows/Microsoft.PowerShell_profile.ps1
 # Vscode
 New-Item -ItemType SymbolicLink -Path $HOME\AppData\Roaming\Code\User -Name settings.json -Value $HOME\dotfiles\dotfiles\vscode\settings.json
 New-Item -ItemType SymbolicLink -Path $HOME\AppData\Roaming\Code\User -Name keybindings.json -Value $HOME\dotfiles\dotfiles\vscode\keybindings.json
