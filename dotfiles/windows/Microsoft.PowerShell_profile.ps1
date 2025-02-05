091469f76e882305d300c12b3ccef6e444653cf2
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-02-05 10:17:03 +0900

    [windows] modify ps1

---
+++ dotfiles/windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "$HOME/dotfiles/dotfiles/starship/config.toml"
--- scripts/install.ps1
+++ scripts/install.ps1
@@ -2,3 +2,5 @@ winget install --id=Starship.Starship  -e
 winget install 7zip.7zip
 winget install --id Google.Chrome
 winget install --id GIMP.GIM
+winget install --id SlackTechnologies.Slack
+winget install --id XPFCC4CD725961
