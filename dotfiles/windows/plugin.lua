47d0b1c110501b257e50a5de1fe46534b4f17cf9
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-02-05 22:16:10 +0900

    [windows] enable starship in cmd

--- dotfiles/windows/plugin.lua
+++ dotfiles/windows/plugin.lua
@@ -1,5 +1,4 @@
 -- starship.lua
 load(io.popen('starship init cmd'):read("*a"))()
-os.setenv('STARSHIP_CONFIG', '$HOME\dotfiles\dotfiles\starship\starship.toml')
-
-io.writeline("plugin load completed.")
+os.setenv('STARSHIP_CONFIG', string.format('%s\\dotfiles\\dotfiles\\starship\\starship.toml', os.getenv("HOME")))
+io.write("plugin load completed.")
