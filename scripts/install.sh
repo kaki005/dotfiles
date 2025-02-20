b0f5762332be331383b7334a5467a286e9ceb62a
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-02-20 20:51:59 +0900

    [zsh] add plugin

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,10 @@ if [ $(uname) = Darwin ]; then
     fi
     brew bundle install --global
     curl https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+    cp -f $DOTFILES_DIR/dotfiles/starship/fonts/0xProtoNerdFont-Regular.ttf ~/Library/Fonts/ # copy font
+    mas install 539883307 # LINE
+    mas install 1114591412 # Memory Clean 2
+    mas install 803453959  # Slack
 fi
 # Install mise dependencies
 if type mise > /dev/null 2>&1; then
