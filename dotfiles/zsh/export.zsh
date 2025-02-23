726c0a246cfb18e91cbadc7e747af766f78e17b9
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-02-23 14:51:37 +0900

    change zcompdump location

--- dotfiles/zsh/export.zsh
+++ dotfiles/zsh/export.zsh
@@ -2,8 +2,7 @@ export EDITOR=nvim
 export RYE_HOME="$HOME/.cache/rye"
 export NVM_DIR="$HOME/.cache/nvm"
 export HIST_DIR="$HOME/.cache/zsh/" # .zshの履歴を保存するファイル
-export HISTFILE="$HOME/.cache/zsh/" # .zshの履歴を保存するファイル
-#export LESSKEY=${DOTFILES_DIR}/dotfiles/less/lesskey
+export HISTFILE="$HOME/.cache/zsh/history.log" # .zshの履歴を保存するファイル
 export LESSHISTFILE="$HOME/.cache/.lesshist"
 export LESS="-i -q -x2 -N -R"
 export LESSCHARSET="utf-8"
@@ -14,6 +13,7 @@ export SAVEHIST=1000000            # 上述のファイルに保存する履歴�
 export MPLCONFIGDIR="$HOME/.cache/matplotlib" # matplotlibのフォントファイル
 export KERAS_HOME="$HOME/.cache/keras" # kerasのHome
 export GOPATH="$HOME/.local/share/go"
+export ZDOTDIR="$HOME/.cache/zsh" # .zcompdumpの置き場所
 #export TLDR_HOME="$HOME/.cache/tldrc" # tldrの置き場
 
 # ****************** C /C++ ********************
