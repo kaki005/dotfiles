fa1c8428131dcdf86f3af234e242b419b8576cb6
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-02-26 18:47:23 +0900

    [eza] add theme

--- dotfiles/zsh/export.zsh
+++ dotfiles/zsh/export.zsh
@@ -1,15 +1,21 @@
-export EDITOR=nvim
-export RYE_HOME="$HOME/.cache/rye"
-export NVM_DIR="$HOME/.cache/nvm"
-export HIST_DIR="$HOME/.cache/zsh/" # .zshの履歴を保存するファイル
-export HISTFILE="$HOME/.cache/zsh/history.log" # .zshの履歴を保存するファイル
 export LESSHISTFILE="$HOME/.cache/.lesshist"
 export LESS="-i -q -x2 -N -R"
 export LESSCHARSET="utf-8"
 export LESSOPEN="| highlight %s"
-export PYTHONSTARTUP="$DOTFILES_DIR/dotfiles/python/.pythonrc.py"
 export HISTSIZE=100000             # メモリ上に保存する履歴のサイズ
 export SAVEHIST=1000000            # 上述のファイルに保存する履歴のサイズ
+
+
+export EDITOR=nvim
+export PAGER=less
+export MANPAGER='nvim +Man!'
+
+export EZA_CONFIG_DIR="$HOME/.config/eza/"
+export RYE_HOME="$HOME/.cache/rye"
+export NVM_DIR="$HOME/.cache/nvm"
+export HIST_DIR="$HOME/.cache/zsh/" # .zshの履歴を保存するファイル
+export HISTFILE="$HOME/.cache/zsh/history.log" # .zshの履歴を保存するファイル
+export PYTHONSTARTUP="$DOTFILES_DIR/dotfiles/python/.pythonrc.py"
 export MPLCONFIGDIR="$HOME/.cache/matplotlib" # matplotlibのフォントファイル
 export KERAS_HOME="$HOME/.cache/keras" # kerasのHome
 export GOPATH="$HOME/.local/share/go"
@@ -22,6 +28,6 @@ export C_INCLUDE_PATH="$MPIPATH/include:$C_INCLUDE_PATH"
 export CPLUS_INCLUDE_PATH="$MPIPATH/include:$CPLUS_INCLUDE_PATH"
 export LIBRARY_PATH="$MPIPATH/lib:$LIBRARY_PATH"
 
-if uname -r | grep -qi microsoft; then
+if uname -r | grep -qi microsoft; then # if wsl
     export WINHOME="/mnt/c/Users/$(wslvar USERNAME)"
 fi
