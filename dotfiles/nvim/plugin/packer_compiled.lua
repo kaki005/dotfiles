39d22324b529b4ba0bef8a6e51b0841602bbca1e
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2025-03-29 09:41:51 +0900

    add cava

--- dotfiles/nvim/plugin/packer_compiled.lua
+++ dotfiles/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1736781742/share/lua/5.1/?.lua;/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1736781742/share/lua/5.1/?/init.lua;/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1736781742/lib/luarocks/rocks-5.1/?.lua;/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1736781742/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1736781742/lib/lua/5.1/?.so"
+local package_path_str = "/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?.lua;/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?/init.lua;/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?.lua;/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/kakiosatsukokorozashi/.cache/nvim/packer_hererocks/2.1.1741730670/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
